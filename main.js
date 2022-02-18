7743975c18ac2cb9e84c50c44e6c970d2cac4310
galipyildiz <galipyildiz9606@outlook.com>
2022-02-18 05:49:38 +0300

    third commit

--- main.js
+++ main.js
@@ -15,40 +15,46 @@ const image = document.getElementById("image");
 booBtn.onclick = function(){
     sound.src = ".\\sounds\\boo.mp3";
     image.src = ".\\images\\booing.gif";
+    document.body.style.backgroundColor = "Coral";
     sound.play();
 }
 applauseBtn.onclick = function(){
     sound.src = ".\\sounds\\applause.mp3";
     image.src = ".\\images\\applause.gif";
+    document.body.style.backgroundColor = "CadetBlue";
     sound.play();
 }
 
 gaspBtn.onclick = function(){
     sound.src = ".\\sounds\\gasp.mp3";
     image.src = ".\\images\\gasping.gif";
+    document.body.style.backgroundColor = "DarkSlateGray";
     sound.play();
 }
 
 tadaBtn.onclick = function(){
     sound.src = ".\\sounds\\tada.mp3";
     image.src = ".\\images\\tada.gif";
+    document.body.style.backgroundColor = "MediumAquaMarine";
     sound.play();
 }
 
 victoryBtn.onclick = function(){
     sound.src = ".\\sounds\\victory.mp3";
     image.src = ".\\images\\victory.gif";
+    document.body.style.backgroundColor = "Tan";
     sound.play();
 }
 
 wrongBtn.onclick = function(){
     sound.src = ".\\sounds\\wrong.mp3";
     image.src = ".\\images\\wrong.gif";
-
+    document.body.style.backgroundColor = "black";
     sound.play();
 }
 sound.onended = function(){
     image.src = ".\\sound.png";
+    document.body.style.backgroundColor = "#a164df";
 }
 
 
